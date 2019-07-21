3ce40bb8826b73868149a5d626de5e6dd7a730da
ken8waru1 <ken8waru1@gmail.com>
2019-07-21 02:21:11 -0700

    Add score, prompt screen, and control legend to UI

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   module: {
     rules: [{
-      test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+      test: /\.(jpe?g|png|wav|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
       loader: 'url-loader?limit=100000'
     }]
   }
